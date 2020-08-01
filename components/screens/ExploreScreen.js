7ddf13a567fcf7057be18d619780646d39493dd7
ArgVampir <kris_.k@hotmail.com>
2020-08-01 22:01:57 +0800

    change categories button design

--- components/screens/ExploreScreen.js
+++ components/screens/ExploreScreen.js
@@ -86,7 +86,11 @@ export default ExploreScreen = ({ navigation }) => {
         <Categories horizontal={true} showsHorizontalScrollIndicator={false}>
           {categoryList.map((category, index) => {
             return (
-              <Category key={index} onPress={() => changeCategory(category)}>
+              <Category
+                key={index}
+                selected={selectedCategory === category ? true : false}
+                onPress={() => changeCategory(category)}
+              >
                 <CategoryName
                   selected={selectedCategory === category ? true : false}
                 >
@@ -149,13 +153,14 @@ const Category = styled.TouchableOpacity`
   align-items: center;
   margin: 0 10px;
   padding: 5px 10px;
-  border-color: #ff9900;
+  background-color: ${(props) => (props.selected ? "#f1b986ff" : "#f3f3f3ff")};
+  border-color: #f1b986ff;
   border-width: 1px;
   border-radius: 20px;
 `;
 
 const CategoryName = styled(Text)`
-  color: ${(props) => (props.selected ? "#ff9900" : "black")};
+  color: ${(props) => (props.selected ? "#fff" : "black")};
   font-weight: ${(props) => (props.selected ? "300" : "300")};
 `;
 
