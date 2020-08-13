a5efb1b50c6cf3734796f6a1118f62568ad52001
ArgVampir <kris_.k@hotmail.com>
2020-08-13 19:21:05 +0200

    standardize explore data text

--- exploreData.js
+++ exploreData.js
@@ -13,7 +13,7 @@ export default places = [
   {
     id: 2,
     title: "Royal Palace",
-    teaser: "A mystical and royal place where all hidden powers are engraved.",
+    teaser: "A mystical and royal place where all hidden powers are engraved",
     description:
       "Royal Palace is packed with street stalls and restaurants the choice of places to eat will be endless, nowhere else in Bangkok will you get to try as many different kinds of Chinese food as you stroll along.",
     rating: 4.9,
