2085a9d733f5b6d65793a8550ac03c08339c8381
ArgVampir <kris_.k@hotmail.com>
2020-08-15 22:53:01 +0800

    call the correct images according to the new assets folder structure

--- tourGuideData.js
+++ tourGuideData.js
@@ -3,7 +3,7 @@ export default tourGuides = [
     id: 1,
     name: "Eunwoo",
     about: "Hello! My name is Eunwoo",
-    nationality: require("./assets/korea.png"),
+    nationality: require("./assets/flags/korea.png"),
     rating: 4.8,
     category: {
       age: 23,
@@ -16,12 +16,12 @@ export default tourGuides = [
       "I was born and raised in both South Korea and fluent in both English and Korean! :) II will be starting a Neurosciences PhD at Stanford University this year (2020). 저는 한국에서 태어나서 미국으로 이민 온 후, 고등학교를 전교1등으로 졸업하고, 노트르담 대학교에서 신경과학을 전공했습니다. 올해 (2020년) 스탠포드 대학교에서 신경과학 박사과정을 시작할 예정이며, 여름방학을 한국어와 영어를 가르치면서 보내려고 합니다. ^^",
     status: "online",
     cover: require("./assets/videoPlayer.png"),
-    photo: require("./assets/eunwoo.png"),
+    photo: require("./assets/tourGuides/eunwoo.png"),
     photos: [
-      require("./assets/eunwoo1.jpg"),
-      require("./assets/eunwoo4.jpg"),
-      require("./assets/eunwoo3.jpg"),
-      require("./assets/eunwoo2.jpg"),
+      require("./assets/tourGuides/eunwoo1.jpg"),
+      require("./assets/tourGuides/eunwoo4.jpg"),
+      require("./assets/tourGuides/eunwoo3.jpg"),
+      require("./assets/tourGuides/eunwoo2.jpg"),
     ],
     tourPlaces: {
       "China Town":
@@ -37,7 +37,7 @@ export default tourGuides = [
     id: 2,
     name: "Suzy Bae",
     about: "About me",
-    nationality: require("./assets/korea.png"),
+    nationality: require("./assets/flags/korea.png"),
     rating: 4.9,
     category: {
       age: 22,
@@ -50,12 +50,12 @@ export default tourGuides = [
       "I studied English in Ireland so I can speak Korean and English Fluently. I have lots of interest so we can talk easily from funny things to serious things. I love playing piano and guitar.",
     status: "online",
     cover: require("./assets/videoPlayer.png"),
-    photo: require("./assets/suzyBae.png"),
+    photo: require("./assets/tourGuides/suzyBae.png"),
     photos: [
-      require("./assets/suzyBae1.jpeg"),
-      require("./assets/suzyBae2.jpeg"),
-      require("./assets/suzyBae3.jpg"),
-      require("./assets/suzyBae4.jpeg"),
+      require("./assets/tourGuides/suzyBae1.jpeg"),
+      require("./assets/tourGuides/suzyBae2.jpeg"),
+      require("./assets/tourGuides/suzyBae3.jpg"),
+      require("./assets/tourGuides/suzyBae4.jpeg"),
     ],
     tourPlaces: {
       "China Town":
@@ -70,7 +70,7 @@ export default tourGuides = [
     id: 3,
     name: "Jisoo",
     about: "Hi~ my name is Jisoo member of BlackPink",
-    nationality: require("./assets/korea.png"),
+    nationality: require("./assets/flags/korea.png"),
     rating: 4.5,
     category: {
       age: 24,
@@ -88,12 +88,12 @@ export default tourGuides = [
       "My name is Jisoo and a native Korean speaker in Seoul. I can speak English and French so if you want to communicate with me in those languages, don't hesitate! I've learned new languages as you guys, so I can understand how difficult it is to get used to a new language. I really love to teach something to other people and talk with various kinds of topics.",
     status: "online",
     cover: require("./assets/videoPlayer.png"),
-    photo: require("./assets/jisoo.png"),
+    photo: require("./assets/tourGuides/jisoo.png"),
     photos: [
-      require("./assets/jisoo1.jpg"),
-      require("./assets/jisoo2.jpeg"),
-      require("./assets/jisoo3.jpg"),
-      require("./assets/jisoo4.jpg"),
+      require("./assets/tourGuides/jisoo1.jpg"),
+      require("./assets/tourGuides/jisoo2.jpeg"),
+      require("./assets/tourGuides/jisoo3.jpg"),
+      require("./assets/tourGuides/jisoo4.jpg"),
     ],
     tourPlaces: {
       "Royal Palace":
@@ -110,7 +110,7 @@ export default tourGuides = [
     id: 4,
     name: "Yangyang",
     about: "I am an actor from China",
-    nationality: require("./assets/china.png"),
+    nationality: require("./assets/flags/china.png"),
     rating: 4.5,
     category: {
       age: 26,
@@ -128,11 +128,11 @@ export default tourGuides = [
       "Hi, everyone! My name is Yangyang. I’m a postgraduate student in Beijing University. My major is an actor in China. I’ve been teaching Chinese Mandarin for about 2 years. I also have lots of experiences in online teaching. I am a very easy-going and patient teacher. Hope you make great progress in studying Chinese. 大家好！我叫晏嘉欣。我是北京师范大学的一名在读研究生。我的专业是汉语国际教育。我喜欢弹古筝，喜欢旅游，喜欢读书。我已经有两年的汉语教学经验了，也有较为丰富的线上汉语教学经验。我是一个和蔼可亲、耐心细致的老师，希望你在我的课堂上学有所成、学有所获。",
     status: "offline",
     cover: require("./assets/videoPlayer.png"),
-    photo: require("./assets/yangyang.png"),
+    photo: require("./assets/tourGuides/yangyang.png"),
     photos: [
-      require("./assets/yangyang1.jpg"),
-      require("./assets/yangyang2.jpg"),
-      require("./assets/yangyang3.jpeg"),
+      require("./assets/tourGuides/yangyang1.jpg"),
+      require("./assets/tourGuides/yangyang2.jpg"),
+      require("./assets/tourGuides/yangyang3.jpeg"),
     ],
     tourPlaces: {
       "Royal Palace":
