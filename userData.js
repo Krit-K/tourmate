7da94f2d84cf3beff10322c98d243f6c082302c7
ArgVampir <kris_.k@hotmail.com>
2020-12-12 04:40:58 -0900

    add user data

---
+++ userData.js
@@ -0,0 +1,128 @@
+export default tourGuides = [
+  {
+    id: 1,
+    name: "Lisa",
+    photo: require("./assets/tourGuides/eunwoo.png"),
+    tourPlaces: {
+      "China Town":
+        "Do you know where is the best place to eat Chicken and Beer? Yes, Yaowarat is the place. You can enjoy delicious food and great view at night. Also souvenirs and temples are located around.. Let's go this awesome place together!",
+      "Wakeboarding at Taco Lake":
+        "Terrific experience for people who loves extreme sport. WakeBoarding is One of the best sporty outdoor activities. It is a community friendly and very chill place for everyone.",
+      "King Rama IX Park":
+        "Who is looking for a natural escape? Come this way. King Rama IX Park have a lovely botany, boat rides on the lake and sounds of wildlife. You would not believe that you were in the city. Perfect picnic place for family, youngsters and lovers.",
+    },
+  },
+
+  {
+    id: 2,
+    name: "Suzy Bae",
+    about: "About me",
+    nationality: require("./assets/flags/korea.png"),
+    rating: 4.9,
+    category: {
+      age: 22,
+      gender: "F",
+      price: 100,
+    },
+    places: ["China Town", "Baiyoke Tower", "Dog in Town Ari"],
+    languages: ["Korean", "English", "Mandarin", "Thai"],
+    description:
+      "I studied English in Ireland so I can speak Korean and English Fluently. I have lots of interest so we can talk easily from funny things to serious things. I love playing piano and guitar.",
+    status: "online",
+    cover: require("./assets/videoPlayer.png"),
+    photo: require("./assets/tourGuides/suzyBae.png"),
+    photos: [
+      require("./assets/tourGuides/suzyBae1.jpeg"),
+      require("./assets/tourGuides/suzyBae2.jpeg"),
+      require("./assets/tourGuides/suzyBae3.jpg"),
+      require("./assets/tourGuides/suzyBae4.jpeg"),
+    ],
+    tourPlaces: {
+      "China Town":
+        "Do you know where is the best place to eat Chicken and Beer? Yes, Yaowarat is the place. You can enjoy delicious food and great view at night. Also souvenirs and temples are located around.. Let's go this awesome place together!",
+      "Baiyoke Tower":
+        "Have you heard about Baiyoke Tower? You can enjoy shopping, food and sightseeing here all at one time! Why don't you look around the tallest building in Thailand!? ",
+      "Dog in Town Ari":
+        "I love dogs do you also love dogs? Who is looking for a cozy cafe with four paws companion, Dog in Town Ari is absolutely great place to spend your afternoon tea at. Dog Lover you are welcome to take picture and plays the dogs along with our amazing coffee and desserts!",
+    },
+  },
+  {
+    id: 3,
+    name: "Jisoo",
+    about: "Hi~ my name is Jisoo member of BlackPink",
+    nationality: require("./assets/flags/korea.png"),
+    rating: 4.5,
+    category: {
+      age: 24,
+      gender: "F",
+      price: 120,
+    },
+    places: [
+      "Royal Palace",
+      "China Town",
+      "King Rama IX Park",
+      "Dog in Town Ari",
+    ],
+    languages: ["Korean", "English", "Mandarin"],
+    description:
+      "My name is Jisoo and a native Korean speaker in Seoul. I can speak English and French so if you want to communicate with me in those languages, don't hesitate! I've learned new languages as you guys, so I can understand how difficult it is to get used to a new language. I really love to teach something to other people and talk with various kinds of topics.",
+    status: "online",
+    cover: require("./assets/videoPlayer.png"),
+    photo: require("./assets/tourGuides/jisoo.png"),
+    photos: [
+      require("./assets/tourGuides/jisoo1.jpg"),
+      require("./assets/tourGuides/jisoo2.jpeg"),
+      require("./assets/tourGuides/jisoo3.jpg"),
+      require("./assets/tourGuides/jisoo4.jpg"),
+    ],
+    tourPlaces: {
+      "Royal Palace":
+        "The dazzling, spectacular Royal Palace is undoubtedly the most famous landmark in Bangkok. Who loves the Historical and Artistic side of Thailand do not missed out to visit this. Gorgeous place to take photos of beautiful architecture and intricate details. ",
+      "China Town":
+        "Do you know where is the best place to eat Chicken and Beer? Yes, Yaowarat is the place. You can enjoy delicious food and great view at night. Also souvenirs and temples are located around.. Let's go this awesome place together!",
+      "King Rama IX Park":
+        "Who is looking for a natural escape? Come this way. King Rama IX Park have a lovely botany, boat rides on the lake and sounds of wildlife. You would not believe that you were in the city. Perfect picnic place for family, youngsters and lovers.",
+      "Dog in Town Ari":
+        "I love dogs do you also love dogs? Who is looking for a cozy cafe with four paws companion, Dog in Town Ari is absolutely great place to spend your afternoon tea at. Dog Lover you are welcome to take picture and plays the dogs along with our amazing coffee and desserts!",
+    },
+  },
+  {
+    id: 4,
+    name: "Yangyang",
+    about: "I am an actor from China",
+    nationality: require("./assets/flags/china.png"),
+    rating: 4.5,
+    category: {
+      age: 26,
+      gender: "M",
+      price: 180,
+    },
+    places: [
+      "Royal Palace",
+      "Damneon Saduak Floating Market",
+      "Baiyoke Tower",
+      "Wakeboarding at Taco Lake",
+    ],
+    languages: ["Mandarin"],
+    description:
+      "Hi, everyone! My name is Yangyang. I’m a postgraduate student in Beijing University. My major is an actor in China. I’ve been teaching Chinese Mandarin for about 2 years. I also have lots of experiences in online teaching. I am a very easy-going and patient teacher. Hope you make great progress in studying Chinese. 大家好！我叫晏嘉欣。我是北京师范大学的一名在读研究生。我的专业是汉语国际教育。我喜欢弹古筝，喜欢旅游，喜欢读书。我已经有两年的汉语教学经验了，也有较为丰富的线上汉语教学经验。我是一个和蔼可亲、耐心细致的老师，希望你在我的课堂上学有所成、学有所获。",
+    status: "offline",
+    cover: require("./assets/videoPlayer.png"),
+    photo: require("./assets/tourGuides/yangyang.png"),
+    photos: [
+      require("./assets/tourGuides/yangyang1.jpg"),
+      require("./assets/tourGuides/yangyang2.jpg"),
+      require("./assets/tourGuides/yangyang3.jpeg"),
+    ],
+    tourPlaces: {
+      "Royal Palace":
+        "The dazzling, spectacular Royal Palace is undoubtedly the most famous landmark in Bangkok. Who loves the Historical and Artistic side of Thailand do not missed out to visit this. Gorgeous place to take photos of beautiful architecture and intricate details. ",
+      "Damneon Saduak Floating Market":
+        "Have you ever ride in a canal that surrounded by numerous street food options. Experiences it with us at the Floating market, a place where you can find cheap local products and friendly locals. Get ready for variety of shooting points!",
+      "Baiyoke Tower":
+        "Have you heard about Baiyoke Tower? You can enjoy shopping, food and sightseeing here all at one time! Why don't you look around the tallest building in Thailand!? ",
+      "Wakeboarding at Taco Lake":
+        "Terrific experience for people who loves extreme sport. WakeBoarding is One of the best sporty outdoor activities. It is a community friendly and very chill place for everyone.",
+    },
+  },
+];
