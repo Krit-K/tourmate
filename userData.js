2073cfa76349778140fda5dcc073b58550549d98
ArgVampir <kris_.k@hotmail.com>
2020-12-17 09:27:51 +0100

    add review component for user details for user2

--- userData.js
+++ userData.js
@@ -16,6 +16,13 @@ export default tourGuides = [
     id: 2,
     name: "Yeji",
     photo: require("./assets/users/yeji.jpeg"),
+    reviews: {
+      "China Town": {
+        rating: 4.0,
+        date: "27/12/20",
+        review:
+          "The place is very nice with streets foods and awesome souveneirs. Highly recommend coming. Very cute and sweet tour guide! We went for a chinese restuarant and the food there was amazing. Took a lots of photo too. I would recommend this place to everyone <3",
+      },
     },
   },
 ];
