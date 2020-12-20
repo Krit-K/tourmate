fcddf5c7a732daa6401cb4601cf61485fb53cb6c
ArgVampir <kris_.k@hotmail.com>
2020-12-20 16:50:00 +0800

    remove reviewCard

--- components/ReviewCard.js
+++ components/ReviewCard.js
@@ -2,11 +2,6 @@ import React from "react";
 import styled from "styled-components/native";
 import { StyleSheet, Platform, StatusBar } from "react-native";
 
-export default reviewCard = (/*name, rating,*/ { source }) => (
-  <ReviewCard>
-    <Image source={source} />
-  </ReviewCard>
-);
 
 const ReviewCard = styled.View``;
 
