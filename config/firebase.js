b353ea38af8ad1237866a7ccb4798a301457e416
ArgVampir <kris_.k@hotmail.com>
2020-12-28 17:22:36 +0800

    create firebase config linking to the firebase account

---
+++ config/firebase.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyAJhF8tEl3sGBLXPYFhvHEeOT0HfERjEaI",
+  authDomain: "thai-tour-app.firebaseapp.com",
+  projectId: "thai-tour-app",
+  storageBucket: "thai-tour-app.appspot.com",
+  messagingSenderId: "267382029521",
+  appId: "1:267382029521:web:09224f3bc31359405d59f0",
+  measurementId: "G-ER8TH2X241",
+};
