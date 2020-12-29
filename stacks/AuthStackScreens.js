daf9e910b65cfec2b1538746007c00c3eaf8025f
ArgVampir <kris_.k@hotmail.com>
2020-12-29 18:03:44 +0800

    fix AuthStackScreen

--- stacks/AuthStackScreens.js
+++ stacks/AuthStackScreens.js
@@ -8,8 +8,8 @@ export default AuthStackScreens = () => {
   const AuthStack = createStackNavigator();
   return (
     <AuthStack.Navigator headerMode="none">
-      <AuthStack.Screen name="SignIn" component={SignInScreen} />
       <AuthStack.Screen name="SignUp" component={SignUpScreen} />
+      <AuthStack.Screen name="SignIn" component={SignInScreen} />
     </AuthStack.Navigator>
   );
 };
