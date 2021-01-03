69e9256fd565825dd50584a5ad0c49257b2395f8
ArgVampir <kris_.k@hotmail.com>
2021-01-03 20:41:30 +0800

    add uploadProfilePhoto to firebase context

--- context/FireBaseContext.js
+++ context/FireBaseContext.js
@@ -34,6 +34,7 @@ const Firebase = {
       });
 
       if (user.profilePhoto) {
+        profilePhotoUrl = await Firebase.uploadProfilePhoto(user.profilePhoto);
       }
 
       delete user.password;
@@ -42,6 +43,46 @@ const Firebase = {
     } catch (error) {
       console.log("Error @createUser: ", error.message);
     }
+  },
+
+  uploadProfilePhoto: async (uri) => {
+    const uid = Firebase.getCurrentUser().uid;
+
+    try {
+      const photo = await Firebase.getBlob(uri);
+
+      const imageRef = firebase.storage().ref("profilePhotos").child(uid);
+
+      await imageRef.put(photo);
+
+      const url = await imageRef.getDownloadURL();
+
+      await db.collection("users").doc(uid).update({
+        profilePhotoUrl: url,
+      });
+
+      return url;
+    } catch (error) {
+      console.log("Error @uploadProfilePhoto: ", error);
+    }
+  },
+
+  getBlob: async (uri) => {
+    return await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+
+      xhr.onload = () => {
+        resolve(xhr.response);
+      };
+
+      xhr.onerror = () => {
+        reject(new TypeError("Network request failed."));
+      };
+
+      xhr.responseType = "blob";
+      xhr.open("GET", uri, true);
+      xhr.send(null);
+    });
   },
 };
 
